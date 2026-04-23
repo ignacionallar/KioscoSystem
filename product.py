44f562b04b519487a02d551b5f2feb64a975faaa
Ignacio-Nallar <ignacioeduardonallar@gmail.com>
2026-04-22 21:38:34 -0300

    i added product methods getters setters

--- product.py
+++ product.py
@@ -1,9 +1,32 @@
-class Producto:
+class Product:
     def __init__(self,name,price,stock):
         if price<0:
             raise ValueError("Precio negativo")
         
-        self.name=name
-        self.price=price
-        self.stock=stock
+        self._name=name
+        self._price=price
+        self._stock=stock
         
+    @property
+    def name(self):
+        return self._name
+    
+    @property
+    def price(self):
+        return self._price
+    
+    @property
+    def stock(self):
+        return self._stock
+    
+    @name.setter
+    def name(self,new_name):
+        self._name=new_name
+
+    @price.setter
+    def price(self,new_price):
+        self._price=new_price
+
+    @stock.setter
+    def stock(self,new_stock):
+        self._stock=new_stock
