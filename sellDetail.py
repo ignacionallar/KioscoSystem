5c01831aa65dd886d86ea6643532b0bfe8c08f90
Ignacio-Nallar <ignacioeduardonallar@gmail.com>
2026-04-27 21:45:47 -0300

    Added sellDetail fuctions, like, property/setters/construct

--- sellDetail.py
+++ sellDetail.py
@@ -1,3 +1,22 @@
+from product import Product
 class SellDetail:
-    def __init__(self):
-        pass
+    def __init__(self, product: Product, quantity):
+        self._product= product
+        self._quantity= quantity
+        self._subtotal=0
+
+    @property
+    def product(self):
+        return self._product
+    
+    @property
+    def quantity(self):
+        return self._quantity
+    
+    @product.setter
+    def product(self, new_product: Product):
+        self._product= new_product
+
+    @quantity.setter
+    def quantity(self, new_quantity):
+        self._quantity= new_quantity
