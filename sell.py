919cee70365df02229d4db830a557e77f2d2d344
Ignacio-Nallar <ignacioeduardonallar@gmail.com>
2026-04-27 21:36:16 -0300

    Added total/item setters

--- sell.py
+++ sell.py
@@ -16,5 +16,18 @@ class Sell:
     @property
     def total(self):
         return self._total
+    
+    @property
+    def items(self):
+        return self._items
+    
+    @total.setter
+    def total(self,new_total):
+        self._total=new_total
+
+    @items.setter
+    def items(self,new_items):
+        self._items=new_items
         
+    
 
