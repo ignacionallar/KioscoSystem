89e8217cde6d334aeae9d773a3e2aa6370e1c217
Ignacio-Nallar <ignacioeduardonallar@gmail.com>
2026-04-27 21:31:49 -0300

    Add calculate_total & property total of sell object

--- sell.py
+++ sell.py
@@ -6,4 +6,15 @@ class Sell:
     def __init__(self):
         self._date= dt.datetime.now().isoformat()
         self._items=[]
+        self._total=0
+
+    def calculate_total(self):
+        for item in item:
+            self._total=item.subtotal+self._total
+        return self._total
+    
+    @property
+    def total(self):
+        return self._total
+        
 
