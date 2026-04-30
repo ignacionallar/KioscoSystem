8431fddd9ab5ef46488b36807ae209b2206f4da9
matias <matiasriosgomez1724@gmail.com>
2026-04-30 14:48:36 -0300

    realice el detalle de venta

--- sellDetail.py
+++ sellDetail.py
@@ -1,3 +1,42 @@
 from product import Product
 class SellDetail:
-    def __init__(self, )
+    def __init__(self, product:Product, quantity ):
+        self._product=product
+        self._quntity=quantity
+        self._subtotal=0
+
+    @property
+    def product(self):
+        return self._product
+    
+    @property
+    def quantity(self):
+        return self._quantity
+    
+    @product.setter
+    def product(self, new_product:Product):
+        self._product=new_product
+
+    @quantity.setter
+    def quantity (self,new_quantity):
+        self._quantity=new_quantity
+
+    def calculate_subtotal(self):
+        x=self.product.price * self._quantity
+        return x
+    
+    @property 
+    def subtotal(self):
+        self._subtotal=self.calculate_subtotal()
+        return self._subtotal
+    
+    subtotal.setter 
+    def subtotal(self, new_subtotal):
+        self._subtotal=new_subtotal
+
+    def to_dict(self):
+        return{
+            "name":self._product.name,
+            "quantity":self._quantity,
+            "subtotal":self._subtotal
+        }
