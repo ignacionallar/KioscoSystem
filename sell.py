07bd4b81a6c0a45f54e38aa06e422092936a1e9f
matias <matiasriosgomez1724@gmail.com>
2026-05-03 16:58:16 -0300

    agrege la clase venta

---
+++ sell.py
@@ -0,0 +1,41 @@
+import datetime as dt 
+from product import Product
+from sellDetail import sellDetail
+
+class Sell:
+    def __init__ (self):
+        self._date=dt.datetime.now().isoformat
+        self._items=[]
+        self._total=0
+
+    def calculate_total(self):
+        total=0
+        for item in self._items:
+            total=item.subtotal+total
+        return total
+    
+    @property 
+    def total(self):
+        self._total=self.calculate_total()
+        return self._total
+    
+    @property
+    def items(self):
+        return self._items
+    
+    @total.setter
+    def items(self, new_total):
+        self._total=new_total
+
+    @items.setter
+    def items(self, new_items):
+        self.items=new_items
+
+    def add_item(self, producto:Product, cantidad):
+        self._items.append(sellDetail(producto,cantidad))
+
+    def to_dict(self):
+        return{
+            "total": self.total,
+            "fecha": self.date
+       }
