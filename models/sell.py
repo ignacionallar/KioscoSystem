d17599e27c0c82a57f231ab242e33341d88fade5
Ignacio-Nallar <ignacioeduardonallar@gmail.com>
2026-05-09 21:48:13 -0300

    Modified sell.py

--- models/sell.py
+++ models/sell.py
@@ -1,41 +1,25 @@
-import datetime as dt
-from product import Product
-from sellDetail import SellDetail
+from typing import Optional, List
+from datetime import datetime
+from sqlmodel import SQLModel, Field, Relationship
 
-class Sell:
-    def __init__(self):
-        self._date= dt.datetime.now().isoformat()
-        self._items=[]
-        self._total=0
+class SellBase(SQLModel):
+    date: datetime = Field(default_factory=datetime.now)
+    total: float = 0.0
 
-    def calculate_total(self):
-        total=0
-        for item in self._items:
-            total=item.subtotal+total
-        return total
-    
-    @property
-    def total(self):
-        self._total=self.calculate_total()
-        return self._total
-    
-    @property
-    def items(self):
-        return self._items
-    
-    @total.setter
-    def total(self,new_total):
-        self._total=new_total
 
-    @items.setter
-    def items(self,new_items):
-        self._items=new_items
-        
-    def add_item(self, producto: Product, cantidad):
-        self._items.append(SellDetail(producto, cantidad))
+class Sell(SellBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    details: List["Item"] = Relationship(back_populates="sell")
 
-    def to_dict(self):
-        return {
-            "total": self.total,
-            "fecha": self._date
-        }
+
+class SellCreate(SellBase):
+    pass
+
+
+class SellRead(SellBase):
+    id: int
+
+
+class SellUpdate(SQLModel):
+    date: Optional[datetime] = None
+    total: Optional[float] = None
