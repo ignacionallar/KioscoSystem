f5cee696acd445daf732959c613dbbb5b6305854
Gonza <gonzalojaviersuarez1@gmail.com>
2026-05-10 23:19:59 -0300

    Producto  repositorio

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from producto import Producto
 from detalleventa import DetalleVenta
 from venta import Venta
+from producto_repositorio import ProductoRepositorio
 
 producto1= Producto("Gaseosa",3000,10)
 producto2= Producto("Hielo",1000,5)
@@ -12,3 +13,28 @@ venta=Venta()
 venta.agregar_detalleventa(producto1,5)
 venta.agregar_detalleventa(producto2,4)
 print(venta.to_dict())
+
+repo=ProductoRepositorio()
+repo.insertar_producto(producto1)
+repo.insertar_producto(producto2)
+
+print("##########################################")
+
+for producto in repo.get_todos_productos():
+    print(producto.to_dict())
+
+print("##########################################")
+
+print("Stock para el producto de id 4")
+stock_consultado = repo.get_stock_productos(4)
+print(stock_consultado)
+print("Eliminar producto id 10")
+
+
+if repo.eliminar_producto(11):
+    print("Producto eliminado con exito")
+else:
+    print("Error al eliminar producto")
+    
+for producto in repo.get_todos_productos():
+    print(producto.to_dict())
--- producto_repositorio.py
+++ producto_repositorio.py
@@ -4,7 +4,7 @@ from producto import Producto
 class ProductoRepositorio:
     def __init__(self,db="kiosco.db"):
         self._db=db
-        self.create_table()
+        self.crear_tabla()
         
     def connect(self):
         return sqlite3.connect(self._db)
@@ -25,17 +25,18 @@ class ProductoRepositorio:
         with self.connect() as con:
             existe = con.execute(
                 "SELECT 1 FROM productos WHERE nombre =?",
-                (producto.nombre)
+                (producto.nombre,)
             ).fetchone()
             
             if existe:
                 return False
             
-            cur= con.exacute("INSERT INTO productos(nombre, precio, stock) values (?,?,?)")
-            (
-                    producto.nombre, producto.precio , producto.stock
-            )
+            cur= con.execute(
             
+            "INSERT INTO productos(nombre, precio, stock) values (?,?,?)",
+            (producto.nombre, producto.precio , producto.stock)
+            
+            )
             return True
     
     def get_todos_productos(self):
@@ -50,14 +51,14 @@ class ProductoRepositorio:
     def get_stock_productos(self, id):
         with self.connect()as con:
             con.row_factory= sqlite3.Row
-            row= con.execute("SELECT stock FROM productos WHERE id=?", (id)).fetchone()
+            row= con.execute("SELECT stock FROM productos WHERE id=?", (id,)).fetchone()
             return row ["stock"] if row else None
         
     def eliminar_producto(self,id):
         try:
             with self.connect() as con:
                 con.row_factory= sqlite3.Row
-                row= con.execute("DELETE FROM productos WHERE id= ?",(id))
+                row= con.execute("DELETE FROM productos WHERE id= ?",(id,))
             return True
         except:
             return False
